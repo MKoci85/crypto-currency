55f53e0650f616ae0440f6fcc8a934a161db3a60
Mariano Koci <mariano.koci@seciu.edu.uy>
2024-08-21 18:05:54 -0300

    Se agrega USDT

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,6 +2,7 @@ import { Currency } from "../types";
 
 export const currencies : Currency[] = [
   { code: 'USD', name: 'Dolar de Estados Unidos'},
+  { code: 'USDT', name: 'Tether'},
   { code: 'EUR', name: 'Euro'},
   { code: 'GBP', name: 'Libra Esterlina'},
   { code: 'ARG', name: 'Peso Argentino'},
